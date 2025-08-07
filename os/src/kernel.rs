84de5be8421723ea342f9de846c894947b76770c
04avis@gmail.com <04avis@gmail.com>
2025-08-06 19:18:43 -0700

    blinking hello world

--- os/src/kernel.rs
+++ os/src/kernel.rs
@@ -1,13 +1,35 @@
-
+use core::{arch::asm, i32};
 static HELLO: &[u8] = b"Hello World!";
 
 pub(crate) fn kernel_loop() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
+    loop{
+        for (i, &byte) in HELLO.iter().enumerate() {
+            unsafe {
+                *vga_buffer.offset(i as isize * 2) = byte;
+                *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            }
+        }
+        sleep();
+        for (i, &_byte) in HELLO.iter().enumerate() {
+            unsafe {
+                *vga_buffer.offset(i as isize * 2) = b' ';
+                *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            }
+        }
+        sleep();
+    }
+}
+
+
+fn sleep() {
+
+    for _ in 0..250000000 {
+        // Or inline assembly if needed
         unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            asm!(
+                "nop"
+            )
         }
     }
-    loop{}
 }

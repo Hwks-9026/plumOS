87631a2c50839440d4d034bc53abb6cd8955d7a0
04avis@gmail.com <04avis@gmail.com>
2025-08-06 19:02:49 -0700

    refactored code to kernel.rs

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,10 +1,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 extern crate core;
-static HELLO: &[u8] = b"Hello World!";
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+mod kernel;
+
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -12,16 +13,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    loop {}
+    kernel::kernel_loop();
 }
 
 #[repr(C, align(8))]

bc2a54c6091f0c1226eb666e422220d092d121d7
04avis@gmail.com <04avis@gmail.com>
2025-09-02 23:23:09 -0600

    moved rust experiments out of the repository. one end goal is still to get rust executables running in plumOS but they're not neccesary in plumOS repo for now.

--- .rust_experiment/run_rust_experiments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ./rust_boot_experiments/
-cargo build -r
-cd ..
-cd rust_experiment_root
-cp ../rust_boot_experiments/target/x86_64-os/release/plumOS ./plumOS.elf
-grub-mkrescue -o ../plum.iso .
-cd ..
-qemu-system-x86_64 -cdrom plum.iso
